cca5136617fe4ee14208c2987e694d32f49d7071
Dwayne Ford <dwayneford@gmail.com>
2015-11-10 08:27:25 -0600

    Update project files

--- gulpfile.js
+++ gulpfile.js
@@ -5,18 +5,31 @@ var minify = require('gulp-uglify');
 var mocha = require('gulp-mocha');
 var rename = require('gulp-rename');
 
+gulp.task('node-package', ['node-package-lib'], function() {
+  return gulp.src('./src/inflector.js')
+  .pipe(rename('js-inflector.js'))
+  .pipe(gulp.dest('./dist/'))
+});
+
+gulp.task('node-package-lib', function() {
+  return gulp.src(['!./src/inflector.js', './src/**'])
+  .pipe(gulp.dest('./dist/'));
+});
+
 gulp.task('package', function() {
   return gulp.src('./src/inflector.js')
   .pipe(browserify())
   .pipe(rename('js-inflector.js'))
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./dist/web'));
 });
+
 gulp.task('package-min', ['package'], function() {
-  return gulp.src('./dist/js-inflector.js')
+  return gulp.src('./dist/web/js-inflector.js')
   .pipe(minify({}))
   .pipe(rename('js-inflector.min.js'))
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./dist/web'));
 });
+
 gulp.task('test', function() {
   global.expect = chai.expect;
   global.STATES = require('./src/defaults/states');
@@ -26,4 +39,4 @@ gulp.task('test', function() {
   .pipe(mocha());
 });
 
-gulp.task('default', ['test', 'package-min']);
+gulp.task('default', ['test', 'package-min', 'node-package']);
